76c8b86b79ee3cc3872eab591c6e2ce20e19c3eb
igor-lourenco <igor.lourencosantos@gmail.com>
2021-11-21 21:28:37 -0300

    Entity Servico criado

---
+++ backend/src/test/java/com/cabeleireiro/CabeleireiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabeleireiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeleireiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
